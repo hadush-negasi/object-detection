9ad9c58ef3b1c590cdc082a81945418f1c710db8
Hadush Negasi <80306906+hadush-negasi@users.noreply.github.com>
2025-09-02 14:10:22 +0300

    Update video_model.py

--- model_utils/video_model.py
+++ model_utils/video_model.py
@@ -3,11 +3,10 @@ import cv2
 import tempfile
 import tensorflow as tf
 import numpy as np
-from utils.model_loader import model, category_index
+from utils.model_loader import get_model
 from utils.drawing import draw_boxes
 
-#model_path = "models/ssd_mobilenet_v2_320x320_coco17_tpu-8"
-#model, category_index = load_model_and_labels(model_path)
+model, category_index = get_model("video") # SSD mobilenet model
 
 def run():
     st.subheader("🎥 Object Detection on Video")
